bbe8fc31a8aaa56d0e18ff5b04a63ee3e2fd41b4
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2025-01-15 13:47:06 -0300

    💄 feat: Estilização do CSS

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,36 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+/* eslint-disable prefer-const */
+import {
+  Globe,
+  GithubLogo,
+  LinkedinLogo,
+} from "@phosphor-icons/react";
 
 function Footer() {
+  let data = new Date().getFullYear();
 
-    let data = new Date().getFullYear()
-
-    return (
-        <>
-            <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex justify-center bg-green-palette-300 text-eme">
+        <div className="container flex flex-col items-center py-4">
+          <p className="text-xl font-bold">
+            Catharina Zagiacomo | Copyright {data}
+          </p>
+          <p className="text-lg">Acesse minhas redes sociais</p>
+          <div className="flex gap-2">
+            <a href="https://linkedin.com/in/czagiacomo/" target="_blank">
+              <LinkedinLogo size={48} weight="regular" />
+            </a>
+            <a href="https://github.com/czagiacomo" target="_blank">
+              <GithubLogo size={48} weight="regular" />
+            </a>
+            <a href="https://czagiacomo.github.io/Portfolio/" target="_blank">
+              <Globe size={48} weight="regular" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,14 @@
+import { Plant } from "@phosphor-icons/react";
+
 function Navbar() {
   return (
     <>
-      <div
-        className="w-full flex justify-center py-4
-            			   bg-indigo-900 text-white"
-      >
+      <div className="w-full flex justify-center py-4 bg-green-palette-300 text-emerald-950 font-semibold">
         <div className="container flex justify-between text-lg">
-          Blog Pessoal
+          <a href="index.html" className="flex items-center">
+            <Plant size={30} color="#3b8c3c" weight="duotone" alt="Home" />
+            <span className="text-xl font-bold ml-2">Blog Pessoal</span>
+          </a>
           <div className="flex gap-4">
             Postagens Temas Cadastrar tema Perfil Sair
           </div>
