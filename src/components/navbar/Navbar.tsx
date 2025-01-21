671167e6cbcd7e2c30765c62038cc6e6e04415d1
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2025-01-21 20:08:42 -0300

    Adicionado página de Perfil do Usuário

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -35,6 +35,9 @@ function Navbar() {
             <Link to="/cadastrartema" className="hover:underline">
               Cadastrar tema
             </Link>
+            <Link to="/perfil" className="hover:underline">
+              Perfil
+            </Link>
             <Link to="" onClick={logout} className="hover:underline">
               Sair
             </Link>
