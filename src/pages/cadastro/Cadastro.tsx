67d25df5ed8460742067b36f712d0c38742252d0
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2025-02-01 16:48:22 -0300

    Update Cadastro.tsx

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,166 +1,240 @@
-import { useState, useEffect, ChangeEvent, FormEvent } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { cadastrarUsuario } from '../../services/Service'
-import { RotatingLines } from 'react-loader-spinner'
-import Usuario from '../../models/Usuario'
+import { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { cadastrarUsuario } from "../../services/Service";
+import { RotatingLines } from "react-loader-spinner";
+import Usuario from "../../models/Usuario";
 
-import './Cadastro.css'
+import "./Cadastro.css";
 
 function Cadastro() {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-
-  const [confirmaSenha, setConfirmaSenha] = useState<string>("")
+  const [confirmaSenha, setConfirmaSenha] = useState<string>("");
 
   const [usuario, setUsuario] = useState<Usuario>({
     id: 0,
-    nome: '',
-    usuario: '',
-    senha: '',
-    foto: ''
-  })
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+  });
 
   useEffect(() => {
     if (usuario.id !== 0) {
-      retornar()
+      retornar();
     }
-  }, [usuario])
+  }, [usuario]);
 
   function retornar() {
-    navigate('/login')
+    navigate("/login");
   }
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
     setUsuario({
       ...usuario,
-      [e.target.name]: e.target.value
-    })
-
+      [e.target.name]: e.target.value,
+    });
   }
 
   function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
-    setConfirmaSenha(e.target.value)
+    setConfirmaSenha(e.target.value);
   }
 
   async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
+    e.preventDefault();
 
     if (confirmaSenha === usuario.senha && usuario.senha.length >= 8) {
-
-      setIsLoading(true)
+      setIsLoading(true);
 
       try {
-        await cadastrarUsuario(`/usuarios/cadastrar`, usuario, setUsuario)
-        alert('Usuário cadastrado com sucesso!')
+        await cadastrarUsuario(`/usuarios/cadastrar`, usuario, setUsuario);
+        alert("Usuário cadastrado com sucesso!");
       } catch (error) {
-        alert('Erro ao cadastrar o usuário!')
+        alert("Erro ao cadastrar o usuário!");
       }
     } else {
-      alert('Dados estão inconsistentes. Verifique as informações do cadastro')
-      setUsuario({ ...usuario, senha: '' })
-      setConfirmaSenha('')
+      alert("Dados estão inconsistentes. Verifique as informações do cadastro");
+      setUsuario({ ...usuario, senha: "" });
+      setConfirmaSenha("");
     }
 
-    setIsLoading(false)
+    setIsLoading(false);
   }
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen 
-            place-items-center font-bold">
+      <div
+        className="grid grid-cols-1 lg:grid-cols-2 h-screen 
+            place-items-center font-bold"
+      >
         <div className="fundoCadastro hidden lg:block"></div>
-        <form className='flex justify-center items-center flex-col w-2/3 gap-3'
-          onSubmit={cadastrarNovoUsuario}>
-          <h2 className='text-slate-900 text-5xl'>Cadastrar</h2>
-          <div className="flex flex-col w-full">
-            <label htmlFor="nome">Nome</label>
-            <input
-              type="text"
-              id="nome"
-              name="nome"
-              placeholder="Nome"
-              className="border-2 border-slate-700 rounded p-2"
-              value={usuario.nome}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            />
+        <form
+          className="flex justify-center items-center flex-col w-2/3 gap-3"
+          onSubmit={cadastrarNovoUsuario}
+        >
+          <h2 className="text-slate-900 text-5xl">Cadastrar</h2>
+          <div className="flex flex-col w-full my-1">
+            <label className="input input-bordered flex items-center gap-2 input-md w-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+              </svg>
+              <input
+                type="text"
+                id="nome"
+                name="nome"
+                className="grow"
+                placeholder="Nome Completo"
+                value={usuario.nome}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  atualizarEstado(e)
+                }
+              />
+            </label>
           </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="usuario">Usuario</label>
-            <input
-              type="text"
-              id="usuario"
-              name="usuario"
-              placeholder="Usuario"
-              className="border-2 border-slate-700 rounded p-2"
-              value={usuario.usuario}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            />
+          <div className="flex flex-col w-full my-1">
+            <label className="input input-bordered flex items-center gap-2 input-md w-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input
+                type="text"
+                id="usuario"
+                name="usuario"
+                className="grow"
+                placeholder="Email de Usuário"
+                value={usuario.usuario}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  atualizarEstado(e)
+                }
+              />
+            </label>
           </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="foto">Foto</label>
-            <input
-              type="text"
-              id="foto"
-              name="foto"
-              placeholder="Foto"
-              className="border-2 border-slate-700 rounded p-2"
-              value={usuario.foto}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            />
+          <div className="flex flex-col w-full my-1">
+            <label className="input input-bordered flex items-center gap-2 input-md w-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M9.5 8.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+                <path
+                  fillRule="evenodd"
+                  d="M2.5 5A1.5 1.5 0 0 0 1 6.5v5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5v-5A1.5 1.5 0 0 0 13.5 5h-.879a1.5 1.5 0 0 1-1.06-.44l-1.122-1.12A1.5 1.5 0 0 0 9.38 3H6.62a1.5 1.5 0 0 0-1.06.44L4.439 4.56A1.5 1.5 0 0 1 3.38 5H2.5ZM11 8.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                type="text"
+                id="foto"
+                name="foto"
+                placeholder="URL da foto de perfil"
+                className="grow"
+                value={usuario.foto}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  atualizarEstado(e)
+                }
+              />
+            </label>
           </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="senha">Senha</label>
-            <input
-              type="password"
-              id="senha"
-              name="senha"
-              placeholder="Senha"
-              className="border-2 border-slate-700 rounded p-2"
-              value={usuario.senha}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            />
+          <></>
+          <div className="flex flex-col w-full my-1">
+            <label className="input input-bordered flex items-center gap-2 input-md w-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                type="password"
+                id="senha"
+                name="senha"
+                placeholder="Digite sua senha"
+                className="grow"
+                value={usuario.senha}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  atualizarEstado(e)
+                }
+              />
+            </label>
           </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="confirmarSenha">Confirmar Senha</label>
-            <input
-              type="password"
-              id="confirmarSenha"
-              name="confirmarSenha"
-              placeholder="Confirmar Senha"
-              className="border-2 border-slate-700 rounded p-2"
-              value={confirmaSenha}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
-            />
+          <div className="flex flex-col w-full my-1">
+            <label className="input input-bordered flex items-center gap-2 input-md w-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                type="password"
+                id="confirmarSenha"
+                name="confirmarSenha"
+                placeholder="Confirme sua Senha"
+                className="grow"
+                value={confirmaSenha}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  handleConfirmarSenha(e)
+                }
+              />
+            </label>
           </div>
           <div className="flex justify-around w-full gap-8">
-            <button className='rounded text-white bg-red-400 
-                  hover:bg-red-700 w-1/2 py-2' onClick={retornar}>
-              Cancelar
-            </button>
-            <button
-              type='submit'
-              className='rounded text-white bg-indigo-400 
-                           hover:bg-indigo-900 w-1/2 py-2
-                           flex justify-center'
-            >
-              {isLoading ? <RotatingLines
-                strokeColor="white"
-                strokeWidth="5"
-                animationDuration="0.75"
-                width="24"
-                visible={true}
-              /> :
-                <span>Cadastrar</span>
-              }
-
-            </button>
+            <div className="flex justify-center w-full p-4">
+              <button
+                className="btn btn-error text-neutral-content font-bold w-1/2 py-2 mr-3"
+                onClick={retornar}
+              >
+                Cancelar
+              </button>
+              <button
+                type="submit"
+                className="btn btn-info text-neutral-content font-bold w-1/2 py-2 ml-3"
+              >
+                {isLoading ? (
+                  <RotatingLines
+                    strokeColor="white"
+                    strokeWidth="5"
+                    animationDuration="0.75"
+                    width="24"
+                    visible={true}
+                  />
+                ) : (
+                  <span>Cadastrar</span>
+                )}
+              </button>
+            </div>
           </div>
         </form>
       </div>
     </>
-  )
+  );
 }
 
-export default Cadastro
+export default Cadastro;
